3b14b7e7f3bd17b6752e4f2ac7eeb3a32b4364f4
Haddouche Maamar <frocodeof@gmail.com>
2024-01-29 23:07:56 +0100

    first commit

---
+++ configure.ts
@@ -0,0 +1,5 @@
+export default {
+  domain: 'frocode.helpshift.com',
+  androidPlatformId: 'frocode_platform_20240120223040494-a7a4ea3eabf3e88',
+  iosPlatformId: 'frocode_platform_20240120223040415-ccc42975592eb45',
+};
